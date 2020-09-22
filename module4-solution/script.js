655f65f8294623890fc132825e93a35c92702d63
SharmaSheetal <69670521+SharmaSheetal@users.noreply.github.com>
2020-09-22 23:02:47 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Sheetal", "John", "Gargi", "Vrushali", "Paul", "Sneha", "Pallavi", "Paula", "Laura", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
